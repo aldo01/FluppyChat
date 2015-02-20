84d44837b71efeeb9751e4971ae501f0c99e2876
Rommanski <dmytro.bohachevskyy@gmail.com>
2015-02-21 00:35:35 +0200

    Login done

--- iOS/FluppyChat/FluppyChat/ViewController.swift
+++ iOS/FluppyChat/FluppyChat/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if (( PFUser.currentUser() ) != nil) {
+        //    makeLogin()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    private func makeLogin() {
+        print("make login\n")
+        self.performSegueWithIdentifier("loginSuccess", sender: self)
+    }
+    
+    @IBAction func loginAction() {
+        let user : PFUser = PFUser()
+        
+        user.username = loginTextField.text
+        user.password = passwordField.text
+        
+        PFUser.logInWithUsernameInBackground(loginTextField.text, password: passwordField.text) { (user:PFUser!, err:NSError!) -> Void in
+            if ( nil == err ) {
+                self.makeLogin()
+            } else {
+                // display error
+                print(err.description)
+                UIAlertView(title: nil, message: err.description, delegate: self, cancelButtonTitle: "Ok").show()
+            }
+        
+        }
+    }
 
 }
 
