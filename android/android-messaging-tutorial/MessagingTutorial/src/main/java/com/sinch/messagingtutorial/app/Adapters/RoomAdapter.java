d30cb7582c855590b0184bce7ba3b77653ccfc21
Rommanski <dmytro.bohachevskyy@gmail.com>
2015-05-24 15:20:49 +0300

    Show people in the room.Room list activity

--- android/android-messaging-tutorial/MessagingTutorial/src/main/java/com/sinch/messagingtutorial/app/Adapters/RoomAdapter.java
+++ android/android-messaging-tutorial/MessagingTutorial/src/main/java/com/sinch/messagingtutorial/app/Adapters/RoomAdapter.java
@@ -1,21 +1,20 @@
 package com.sinch.messagingtutorial.app.Adapters;
 
+import java.util.Arrays;
 import java.util.List;
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ListView;
 
+import com.parse.FindCallback;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.sinch.android.rtc.messaging.WritableMessage;
 import com.sinch.messagingtutorial.app.R;
 
 import android.content.Context;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
@@ -37,11 +36,42 @@ public class RoomAdapter extends ArrayAdapter<ParseObject> {
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View rowView = inflater.inflate( R.layout.user_list_item, parent, false);
 
-        ParseObject obj = objLsit.get(position);
-        
         // set information in cell
         final TextView nameTextView = (TextView) rowView.findViewById( R.id.userListItem );
-        nameTextView.setText( "Room X" );
+        nameTextView.setText( "Friends" );
+
+        ParseObject obj = objLsit.get(position);
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("PeopleInRoom");
+        query.whereEqualTo("room", obj);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> list, com.parse.ParseException e) {
+                if (e == null) {
+                    String title = "";
+
+                    for ( ParseObject o : list) {
+
+                        String name = "";
+                        try {
+                            ParseUser u = o.getParseUser("people");
+                            name = u.fetchIfNeeded().getString("username");
+                            title += name + ", ";
+                        } catch (com.parse.ParseException err) {
+                            Log.v("PARSE_ERROR", err.toString());
+                            e.printStackTrace();
+                        }
+                    }
+
+                    nameTextView.setText( title );
+                } else {
+                    Log.d("score", "Error: " + e.getMessage());
+                }
+            }
+        });
+
+
+
 
         return rowView;
     }
