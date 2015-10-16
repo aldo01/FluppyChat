7c07ac9e7185b6ed5b8497161b5f3ea50dc850df
Dmytro Bohachevskyy <dmytro.bohachevskyy@gmail.com>
2015-10-17 00:06:17 +0300

    Added worckspace files

--- ios/FluppyChat/FluppyChat/AppDelegate.swift
+++ ios/FluppyChat/FluppyChat/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         // init parse application
-        Parse.setApplicationId("fYiaMJQcSGKjQB3AwhpGmpFoBvE8UiLJAAQMGKjh", clientKey: "t5lfmPcRZjfRHnBGlPYS984ahstd1nHriMdirpA9")
+        Parse.setApplicationId("***", clientKey: "***")
         
         PhotoContainer.photosDic = [:]
         
